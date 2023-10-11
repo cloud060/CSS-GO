87d3ec93cf79f0cb8f9d92c3b0ed45b699129d5e
GERTYRE <denisgoricuk@gmail.com>
2023-10-11 13:45:18 +0300

    Update 1
    1

---
+++ Site_BooStrap_Tema_Gamers-main/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            mensagem: {
+                required: true,
+            }
+        }
+    })
+});
